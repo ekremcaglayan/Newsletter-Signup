3682edcda0f818415d2f83caa6769bd875faf4a1
ekremcaglayan <ekremcaglayan1@hotmail.com>
2023-02-20 18:22:13 +0300

    posting data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
@@ -11,13 +12,45 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    var firstName = req.body.fName;
-    var lastNAme = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastNAme = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastNAme, email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastNAme,
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us21.api.mailchimp.com/3.0/lists/36e35d1abe";
+    const options = {
+        method: "POST",
+        auth: "ekrem:2e9390cf86e597b9319644d1d6b77c7c-us21",
+
+    };
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
 });
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000.");
 });
+
+// 2e9390cf86e597b9319644d1d6b77c7c-us21
+// 36e35d1abe
